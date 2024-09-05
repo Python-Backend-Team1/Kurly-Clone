436e26c4a47bd838af0239a66b8f079453bbc88f
pandalove77 <h3861411@naver.com>
2024-09-05 14:32:48 +0900

    update

--- kurlyclone/settings.py
+++ kurlyclone/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-#     'product',
+    'product',
 #     'category',
 #     'orders',
 #     'orderitem',
