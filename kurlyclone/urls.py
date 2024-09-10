9cf28ed2353138665c593333673214fec9a13116
pandalove77 <h3861411@naver.com>
2024-09-10 11:39:01 +0900

    add mypage

--- kurlyclone/urls.py
+++ kurlyclone/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.urls import path
 from django.contrib import admin
 from django.urls import path, include
-from users.views import CustomPasswordResetView ,login_view, home_view,signup_view ,find_username_view,mypage_view                              # home_view 임포트 추가
+from users.views import CustomPasswordResetView ,login_view, home_view,signup_view ,find_username_view,mypage_view
 from django.contrib.auth import views as auth_views                                      #이메일인증(비밀번호찾기관련)
 
  
@@ -30,6 +30,7 @@ urlpatterns = [
     path('signup/', signup_view, name='signup'),                             # 회원가입 URL 추가
     path('mypage/', mypage_view, name='mypage'),                               #마이페이지
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),        #로그아웃뷰
+    path('mypage/', mypage_view, name='mypage'),
     path('', home_view, name='home'),                                      # 루트 URL 패턴 추가
     
     path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),  # 여기서 변경
--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,15 @@
 # users/urls.py
 
+from . import views
 from django.urls import path
-from .views import CustomPasswordResetView ,login_view, home_view , signup_view , find_username_view,mypage_view
+from .views import CustomPasswordResetView ,login_view, home_view , signup_view , find_username_view
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', login_view, name='login'),        #로그인
     path('home/', home_view, name='home'),          #홈페이지
     path('signup/', signup_view, name='signup'),  # 회원가입 URL
-     path('mypage/', mypage_view, name='mypage'), #마이 페이지 
+    path('mypage/', views.mypage_view, name='mypage'), 
      path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('find-username/', find_username_view, name='find_username'),                
     path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),  # 여기서 변경
